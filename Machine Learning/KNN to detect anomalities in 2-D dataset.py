00a7716f02421c1084947fff92c7244b378c1fe8
Mahima 🦄 <93143005+cosmiclust@users.noreply.github.com>
2025-07-11 23:57:26 +0530

    Update KNN to detect anomalities in 2-D dataset.py

--- Machine Learning/KNN to detect anomalities in 2-D dataset.py
+++ Machine Learning/KNN to detect anomalities in 2-D dataset.py
@@ -20,3 +20,11 @@ anomalies = data[np.abs(z_scores) > threshold] #data points having z-scores > 3
     here we won't use z-score or IQR for 2-D dataset we will do KNN.
     2-D datset means -> [(x1,y1),(x2,y2),(x3,y3)....]
 '''
+import numpy as np
+data = np.array([1,2],[3,3],[4,5],[6,7])
+# Euclidean Distance Calculation
+def eud_dist_cal (a,b):        # here a, b means a = [x1,y1] & b = [x2,y2]
+  return np.sqrt(np.sum((a-b)**2))
+# Calculating the point which is farthest
+
+  
